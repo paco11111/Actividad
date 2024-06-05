1a736e9d1eed397228b153f2b62f574421b7080c
paco11111 <152623847+paco11111@users.noreply.github.com>
2024-06-05 18:24:28 +0200

    Update Ejemplo.java

--- ejemplo/src/main/java/com/mycompany/ejemplo/Ejemplo.java
+++ ejemplo/src/main/java/com/mycompany/ejemplo/Ejemplo.java
@@ -11,6 +11,6 @@ package com.mycompany.ejemplo;
 public class Ejemplo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola");
     }
 }
