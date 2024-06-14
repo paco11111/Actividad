005b06b5e2592e8742426ce8c82d5d0f8308725f
dam119 <dam119@IF01-19.iesmhp.local>
2024-06-14 15:11:54 +0200

    a

---
+++ hola/src/main/java/com/mycompany/hola/Hola.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hola;
+
+/**
+ *
+ * @author dam119
+ */
+public class Hola {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
